b55c70fc6d2aa099d0fc694de0119bf773e88e02
Wenlong Ni <wen@microsoft.com>
2017-08-19 12:34:43 -0700

    first commit

---
+++ GeekQuiz/Scripts/app/quiz-controller.js
@@ -0,0 +1,43 @@
+﻿angular.module('QuizApp', [])
+    .controller('QuizCtrl', function ($scope, $http) {
+        $scope.answered = false;
+        $scope.title = "loading question...";
+        $scope.options = [];
+        $scope.correctAnswer = false;
+        $scope.working = false;
+
+        $scope.answer = function () {
+            return $scope.correctAnswer ? 'correct' : 'incorrect';
+        };
+
+        $scope.nextQuestion = function () {
+            $scope.working = true;
+
+            $scope.answered = false;
+            $scope.title = "loading question...";
+            $scope.options = [];
+
+            $http.get("/api/trivia").then(function (response) { //data, status, headers, config) {
+                $scope.options = response.data.options;
+                $scope.title = response.data.title;
+                $scope.answered = false;
+                $scope.working = false;
+            }).catch(function (response) {
+                $scope.title = "Oops... something went wrong";
+                $scope.working = false;
+            });
+        };
+
+        $scope.sendAnswer = function (option) {
+            $scope.working = true;
+            $scope.answered = true;
+
+            $http.post('/api/trivia', { 'questionId': option.questionId, 'optionId': option.id }).then(function (response) {
+                $scope.correctAnswer = (response.data === true);
+                $scope.working = false;
+            }).catch(function (response) {
+                $scope.title = "Oops... something went wrong";
+                $scope.working = false;
+            });
+        };
+    });
